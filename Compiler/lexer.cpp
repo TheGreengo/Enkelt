ea722366a832e8192d51a6df2df29b190112cde4
Braden <bradenoptimus@gmail.com>
2023-10-20 11:51:28 -0700

    .gitignore added

--- Compiler/lexer.cpp
+++ Compiler/lexer.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include "token.cpp"
 
-// ok, there
-// TODO: 
+// OK, before I get too far along, I need to think about how this actually is going to work
+// For this lexer, here's my thought, it takes each char and then checks what tokens 
+
 class Lexer {
     std::ifstream file;
+    std::vector<TokenType> tokens;
 
     Lexer(std::string loadFile) {
         file = std::ifstream(loadFile);
@@ -15,12 +19,18 @@ class Lexer {
         file.close(); 
     }
 
-    void run() {
+    std::vector<TokenType> run() {
         char curr;
     
         while (curr != EOF) {
             curr = file.get();
             std::cout << curr;
         }
+
+        return tokens;
+    }
+
+    bool isWhite(char curr) {
+        return curr == ' ' || curr == '\t' || curr == '\n' || curr == '\r';
     }
 };
--- Compiler/token.cpp
+++ Compiler/token.cpp
@@ -9,10 +9,130 @@ enum TokenType {
     TYPE,
     KEY_WORD,
     VAR_NAME,
-    FUNC_NAME
+    MULT_SYM,
+    ADD_SYM,
+    SUB_SYM,
+    EQU_SYM,
+    LES_SYM,
+    GRT_SYM,
+    COMMA,
+    PERIOD,
+    L_PAR,
+    R_PAR,
+    SEMI_COLON,
+    COLON,
+    L_BRACK,
+    R_BRACK,
+    L_BRACE,
+    R_BRACE,
+    FORW_SL,
+    BACK_SL,
+    D_QUOTE,
+    S_QUOTE,
+    BAR
 }
 
 class Token {
     std::string name;
+    TokenType type;
 
+    Token() { this->name, this->type };
+
+    std::string toString() {
+        std::string result = "";
+        result.append(name);
+        switch (type)
+        {
+        case INT:
+            result.append(": INT");
+            break;
+        case FLOAT:
+            result.append(": FLOAT");
+            break;
+        case CHAR:
+            result.append(": CHAR");
+            break;
+        case STRING:
+            result.append(": STRING");
+            break;
+        case BOOL:
+            result.append(": BOOL");
+            break;
+        case TYPE:
+            result.append(": TYPE");
+            break;
+        case KEY_WORD:
+            result.append(": KEY_WORD");
+            break;
+        case VAR_NAME:
+            result.append(": VAR_NAME");
+            break;
+        case MULT_SYM:
+            result.append(": MULT_SYM");
+            break;
+        case ADD_SYM:
+            result.append(": ADD__SYM");
+            break;
+        case SUB_SYM:
+            result.append(": SUB_SYM");
+            break;
+        case EQU_SYM:
+            result.append(": EQU_SYM");
+            break;
+        case LES_SYM:
+            result.append(": LES_SYM");
+            break;
+        case GRT_SYM:
+            result.append(": GRT_SYM");
+            break;
+        case COMMA:
+            result.append(": COMMA");
+            break;
+        case PERIOD:
+            result.append(": PERIOD");
+            break;
+        case L_PAR:
+            result.append(": L_PAR");
+            break;
+        case R_PAR:
+            result.append(": R_PAR");
+            break;
+        case SEMI_COLON:
+            result.append(": SEMI_COLON");
+            break;
+        case COLON:
+            result.append(": COLON");
+            break;
+        case L_BRACK:
+            result.append(": L_BRACK");
+            break;
+        case R_BRACK:
+            result.append(": R_BRACK");
+            break;
+        case L_BRACE:
+            result.append(": L_BRACE");
+            break;
+        case R_BRACE:
+            result.append(": R_BRACE");
+            break;
+        case FORW_SL:
+            result.append(": FORW_SL");
+            break;
+        case BACK_SL:
+            result.append(": BACK_SL");
+            break;
+        case D_QUOTE:
+            result.append(": D_QUOTE");
+            break;
+        case S_QUOTE:
+            result.append(": S_QUOTE");
+            break;
+        case BAR:
+            result.append(": BAR");
+            break;
+        default:
+            result.append("INVALID");
+            break;
+        }
+    }
 };
