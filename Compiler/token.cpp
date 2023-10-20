009ccfa9430bf0f5da615d2af41b1e1233994d4f
Braden <bradenoptimus@gmail.com>
2023-10-20 11:18:24 -0700

    token starting

--- Compiler/token.cpp
+++ Compiler/token.cpp
@@ -1,5 +1,18 @@
 #include <string>
 
+enum TokenType {
+    INT,
+    FLOAT,
+    CHAR,
+    STRING,
+    BOOL,
+    TYPE,
+    KEY_WORD,
+    VAR_NAME,
+    FUNC_NAME
+}
+
 class Token {
     std::string name;
+
 };
