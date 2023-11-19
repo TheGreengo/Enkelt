d2a3b86f0ef69a732c298429218122a1acc7140a
Braden <bradenoptimus@gmail.com>
2023-11-18 21:53:28 -0700

    creating a notes page basically

--- Compiler/main.cpp
+++ Compiler/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char ** argv) {
     if (argc < 2) {
         std::cout << "Error: please provide name of file to be compiled." \
         << std::endl;
+        return 1;
     }
 
     std::cout << "Enkelt compiler starting" << std::endl;
